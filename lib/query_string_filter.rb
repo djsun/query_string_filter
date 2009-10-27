eda72fa7b799b606639d592adf5e7eebf98b432f
David James <djames@sunlightfoundation.com>
2009-10-27 17:03:43 -0400

    handle strings and underscores better

--- lib/query_string_filter.rb
+++ lib/query_string_filter.rb
@@ -4,12 +4,16 @@ Treetop.load(File.dirname(__FILE__) + '/filter')
 
 class QueryStringFilter
   
+  class ParseError < RuntimeError; end
+  
   def initialize
     @parser = FilterParser.new
   end
   
-  def process(string)
-    @parser.parse(string).eval
+  def parse(string)
+    parsed = @parser.parse(string)
+    raise ParseError, "unable to parse: #{string}" unless parsed
+    parsed.eval
   end
   
 end
--- spec/query_string_filter_spec.rb
+++ spec/query_string_filter_spec.rb
@@ -6,56 +6,87 @@ describe "QueryStringFilter" do
     @filter = QueryStringFilter.new
   end
   
-  describe "basic" do
+  describe "failure" do
+    it "no operator" do
+      lambda {
+        @filter.parse("something")
+      }.should raise_error(QueryStringFilter::ParseError)
+    end
+  end
   
+  describe "keys" do
+    it "key with underscore" do
+      @filter.parse("with_underscore = test").should == {
+        :with_underscore => "test"
+      }
+    end
+
+    it "value with single quotes" do
+      @filter.parse("foo = 'bar camp'").should == {
+        :foo => "bar camp"
+      }
+    end
+  end
+  
+  describe "basic operators" do
     it ": with string" do
       expected = {
         :name => /david/
       }
       ["name:david", "name : david"].each do |s|
-        @filter.process(s).should == expected
+        @filter.parse(s).should == expected
       end
     end
   
     it ": with number" do
       expected = { :count => /77/ }
       ["count:77", "count : 77"].each do |s|
-        @filter.process(s).should == expected
+        @filter.parse(s).should == expected
+      end
+    end
+
+    it "= with string" do
+      expected = {
+        :name => "David"
+      }
+      [
+        %(name=David),
+        %(name = David)
+      ].each do |s|
+        @filter.parse(s).should == expected
       end
     end
   
     it ">" do
       expected = { :value => { '$gt' => 12 } }
       ["value>12", "value > 12"].each do |s|
-        @filter.process(s).should == expected
+        @filter.parse(s).should == expected
       end
     end
   
     it "<" do
       expected = { :value => { '$lt' => 12 } }
       ["value<12", "value < 12"].each do |s|
-        @filter.process(s).should == expected
+        @filter.parse(s).should == expected
       end
     end
   
     it ">=" do
       expected = { :value => { '$gte' => 12 } }
       ["value>=12", "value >= 12"].each do |s|
-        @filter.process(s).should == expected
+        @filter.parse(s).should == expected
       end
     end
   
     it "<=" do
       expected = { :value => { '$lte' => 12 } }
       ["value<=12", "value <= 12"].each do |s|
-        @filter.process(s).should == expected
+        @filter.parse(s).should == expected
       end
     end
-    
   end
   
   describe "compound" do
-    
     it ": with 2 strings" do
       expected = {
         :title => /election|texas/
@@ -64,7 +95,7 @@ describe "QueryStringFilter" do
         "title:election,texas",
         "title : election , texas"
       ].each do |s|
-        @filter.process(s).should == expected
+        @filter.parse(s).should == expected
       end
     end
 
@@ -75,10 +106,9 @@ describe "QueryStringFilter" do
       [
         "title:election,texas,bob",
       ].each do |s|
-        @filter.process(s).should == expected
+        @filter.parse(s).should == expected
       end
     end
-    
   end
 
 end
